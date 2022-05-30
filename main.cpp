62db4411da6eb1477e2f194d82523921666298ab
felipecanias <felacanas2003@gmail.com>
2022-05-30 17:19:46 -0300

    Cambios Profesionales

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include "validacion.cpp"
+#include "profesionales.cpp"
 using namespace std;
 
 int main(){
-  VALIDACION V;
-  string val;
 
-  while(true){
-    cout << "Ingrese su DNI: "; getline(cin, val);
-    cout << V.CharOnly(val) << endl;
-  }
+
   return 0;
 }
