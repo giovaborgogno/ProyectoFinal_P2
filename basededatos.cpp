3c5742a94e92c9f12cdf2a8ce2181278de392a81
giovaborgogno <giovaborgogno@gmail.com>
2022-05-31 20:09:49 -0300

    Metodos para crear los archivos.csv

--- basededatos.cpp
+++ basededatos.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "basededatos.h"
 using namespace std;
 
@@ -18,4 +19,57 @@ void BASEDEDATOS::AgregarPROFESIONAL(string Nombre, string Apellido, string Emai
 void BASEDEDATOS::AgregarADMINISTRATIVO(string Nombre, string Apellido, int Dni, string Email, string Puesto, float Sueldo){
   AdminDB[AdminDBIndex] = new ADMINISTRATIVO(Nombre,Apellido,Dni,Email,Puesto,Sueldo);
   AdminDBIndex++;
+}
+
+void BASEDEDATOS::PrintADMINISTRATIVOS(){
+ofstream administrativoscsv;
+
+    administrativoscsv.open("Administrativos.csv", ios::out);
+
+    administrativoscsv << "Nro de Cuenta,Estado,DNI,Apellido,Nombre,Puesto,Sueldo,Saldo,Email"<<endl;
+        for (int x = 0; x < AdminDBIndex; x++)
+    {
+        administrativoscsv <<AdminDB[x]->C->getNumeroDeCuenta() << "," << AdminDB[x]->C->getEstado() << "," << AdminDB[x]->getDni() << "," << AdminDB[x]->getApellido()<< "," << AdminDB[x]->getNombre() << "," << AdminDB[x]->getPuesto()<< "," << AdminDB[x]->getSueldo()<< "," << AdminDB[x]->C->getSaldo()<< "," << AdminDB[x]->getEmail() << endl;
+    }
+    administrativoscsv.close();
+}
+
+void BASEDEDATOS::PrintClientesDeAlta(){
+ofstream altacsv;
+
+    altacsv.open("ClientesDeAlta.csv", ios::out);
+
+    altacsv << "Nro de Cuenta,DNI,Apellido,Nombre,Sueldo,Saldo,Email"<<endl;
+    
+        for (int x = 0; x < AdminDBIndex; x++)
+    {
+      if(AdminDB[x]->C->getEstado()=='A')
+        altacsv <<AdminDB[x]->C->getNumeroDeCuenta() <<  "," << AdminDB[x]->getDni() << "," << AdminDB[x]->getApellido()<< "," << AdminDB[x]->getNombre() << ","  << AdminDB[x]->getSueldo()<< "," << AdminDB[x]->C->getSaldo()<< "," << AdminDB[x]->getEmail() << endl;
+    }
+            for (int x = 0; x < ProfDBIndex; x++)
+    {
+      if(ProfDB[x]->C->getEstado()=='A')
+        altacsv <<ProfDB[x]->C->getNumeroDeCuenta() <<  "," << ProfDB[x]->getDni() << "," << ProfDB[x]->getApellido()<< "," << ProfDB[x]->getNombre() << ","  << ProfDB[x]->getSueldo()<< "," << ProfDB[x]->C->getSaldo()<< "," << ProfDB[x]->getEmail() << endl;
+    }
+    altacsv.close();
+}
+
+void BASEDEDATOS::PrintClientesDeBaja(){
+ofstream bajacsv;
+
+    bajacsv.open("ClientesDeBaja.csv", ios::out);
+
+    bajacsv << "Nro de Cuenta,DNI,Apellido,Nombre,Sueldo,Saldo,Email"<<endl;
+    
+        for (int x = 0; x < AdminDBIndex; x++)
+    {
+      if(AdminDB[x]->C->getEstado()=='B')
+        bajacsv <<AdminDB[x]->C->getNumeroDeCuenta() <<  "," << AdminDB[x]->getDni() << "," << AdminDB[x]->getApellido()<< "," << AdminDB[x]->getNombre() << ","  << AdminDB[x]->getSueldo()<< "," << AdminDB[x]->C->getSaldo()<< "," << AdminDB[x]->getEmail() << endl;
+    }
+            for (int x = 0; x < ProfDBIndex; x++)
+    {
+      if(ProfDB[x]->C->getEstado()=='B')
+        bajacsv <<ProfDB[x]->C->getNumeroDeCuenta() <<  "," << ProfDB[x]->getDni() << "," << ProfDB[x]->getApellido()<< "," << ProfDB[x]->getNombre() << ","  << ProfDB[x]->getSueldo()<< "," << ProfDB[x]->C->getSaldo()<< "," << ProfDB[x]->getEmail() << endl;
+    }
+    bajacsv.close();
 }
--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ using namespace std;
 int main(){
 
   BASEDEDATOS DB(3);
-  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Giganta","Ehhh",4,8000000);
-  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Giganta","Ehhh",4,8000000);
-  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Giganta","Ehhh",4,8000000);
-  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Giganta","Ehhh",4,8000000);
-  DB.ProfDB[3]->C->switchEstado();
-  DB.ProfDB[2]->C->setEstado('E');
-  cout << DB.ProfDB[3]->C->getEstado();
-  cout << DB.ProfDB[1]->C->getEstado();
-  cout << DB.ProfDB[2]->C->getEstado() << endl;
+  DB.AgregarADMINISTRATIVO("Santi","Lucero",44653137,"Email@gmail.com","Jefe" ,8000000);
+  DB.AgregarADMINISTRATIVO("Santi","Lucero",44653137,"Email@gmail.com","Jefe" ,8000000);
+  DB.AgregarADMINISTRATIVO("Santi","Lucero",44653137,"Email@gmail.com","Jefe" ,8000000);
+  DB.AgregarADMINISTRATIVO("Santi","Lucero",44653137,"Email@gmail.com","Jefe" ,8000000);
+  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Ingeniero", "jefesito",4 ,8000000);
+  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Ingeniero", "jefesito",4 ,8000000);
+  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Ingeniero", "jefesito",4 ,8000000);
+  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Ingeniero", "jefesito",4 ,8000000);
+  DB.AdminDB[1]->C->switchEstado();
+  DB.AdminDB[3]->C->switchEstado();
   DB.ProfDB[1]->C->switchEstado();
-  cout << DB.ProfDB[0]->C->getEstado();
-  cout << DB.ProfDB[1]->C->getEstado();
-  cout << DB.ProfDB[2]->C->getEstado();
-
+  DB.ProfDB[3]->C->switchEstado();
+  DB.PrintClientesDeAlta();
+  DB.PrintClientesDeBaja();
   return 0;
 }
