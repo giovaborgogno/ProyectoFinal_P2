1140a00379b1da9c7f2a22184e3c23150804eeda
MaxiLR <maximolr.itr@gmail.com>
2022-05-31 18:40:01 -0300

    Merge branch 'santi' into maxi, sin asignar metodos al menu

--- programa.h
+++ programa.h
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "validacion.cpp"
+//#include "administrativos.cpp"
+//#include "profesionales.cpp"
+
 using namespace std;
 
 class PROGRAMA{
-  
+  private:
+    VALIDACION *Verify;
+    //PROFESIONAL *Prof;
+    //ADMINISTRATIVO *Admin;
+
+  public:
+    int Convert(string v);
+    void Start();
+
 };
