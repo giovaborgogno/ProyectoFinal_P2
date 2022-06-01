a0bf0988696787d41866a016f1b3384966dcaec5
felipecanias <felacanas2003@gmail.com>
2022-06-01 13:30:32 -0300

    Reorganizacion del directorio y definicion de los metodos de la clase BASEDEDATOS

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "validacion.cpp"
-#include "basededatos.cpp"
+#include "assets/validacion.cpp"
+#include "assets/basededatos.cpp"
 
 using namespace std;
 
@@ -11,15 +11,14 @@ int main(){
   DB.AgregarADMINISTRATIVO("Santi","Lucero",44653137,"Email@gmail.com","Jefe" ,8000000);
   DB.AgregarADMINISTRATIVO("Santi","Lucero",44653137,"Email@gmail.com","Jefe" ,8000000);
   DB.AgregarADMINISTRATIVO("Santi","Lucero",44653137,"Email@gmail.com","Jefe" ,8000000);
-  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Ingeniero", "jefesito",4 ,8000000);
-  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Ingeniero", "jefesito",4 ,8000000);
-  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Ingeniero", "jefesito",4 ,8000000);
-  DB.AgregarPROFESIONAL("Santi","Lucero","Email@gmail.com",44653137,"Ingeniero", "jefesito",4 ,8000000);
+  DB.AgregarPROFESIONAL("Santi","Lucero",44653137,"Email@gmail.com","Ingeniero", "jefesito",4 ,8000000);
+  DB.AgregarPROFESIONAL("Santi","Lucero",44653137,"Email@gmail.com","Ingeniero", "jefesito",4 ,8000000);
+  DB.AgregarPROFESIONAL("Santi","Lucero",44653137,"Email@gmail.com","Ingeniero", "jefesito",4 ,8000000);
+  DB.AgregarPROFESIONAL("Santi","Lucero",44653137,"Email@gmail.com","Ingeniero", "jefesito",4 ,8000000);
   DB.AdminDB[1]->C->switchEstado();
   DB.AdminDB[3]->C->switchEstado();
   DB.ProfDB[1]->C->switchEstado();
   DB.ProfDB[3]->C->switchEstado();
-  DB.PrintClientesDeAlta();
-  DB.PrintClientesDeBaja();
+  DB.PrintPROFESIONALES();
   return 0;
 }
